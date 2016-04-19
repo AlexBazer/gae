dc41e5b275c927a14a035d2bcbae2e5bdc970d2d
Alexandr Balagura <davnobezimeni@gmail.com>
2016-04-19 22:50:13 +0300

    Play with datastore

--- app/app.py
+++ app/app.py
@@ -1,4 +1,22 @@
 import webapp2
+from google.appengine.ext import ndb
+
+TASK_LIST = 'task_list'
+
+def get_task_list_key():
+    return ndb.Key('TaskManager', TASK_LIST)
+
+class Task(ndb.Model):
+    content = ndb.StringProperty()
+    finished = ndb.BooleanProperty()
+    dtime = ndb.DateTimeProperty(auto_now_add=True)
+
+    @classmethod
+    def get_tasks(cls, task_list):
+        return cls.query(ancestor=task_list).order(-cls.dtime)
+    @classmethod
+    def get_entity(cls, task_list, _id):
+        return ndb.Key(cls._class_name(), _id, parent=task_list).get()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
