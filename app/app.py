8500824fa6be427dca9197139dbf351aa706bc98
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-20 08:32:27 +0300

    Correct visual indents

--- app/app.py
+++ app/app.py
@@ -5,8 +5,10 @@ import json
 
 TASK_LIST = 'task_list'
 
+
 def get_task_list_key():
     return ndb.Key('TaskManager', TASK_LIST)
+
 
 class Task(ndb.Model):
     content = ndb.StringProperty()
@@ -16,14 +18,17 @@ class Task(ndb.Model):
     @classmethod
     def get_tasks(cls, task_list):
         return cls.query(ancestor=task_list).order(-cls.dtime).fetch()
+
     @classmethod
     def get_entity(cls, task_list, _id):
         return ndb.Key(cls._class_name(), _id, parent=task_list).get()
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+
 
 class TaskListHandler(webapp2.RequestHandler):
     """Handle tasks list
@@ -32,11 +37,13 @@ class TaskListHandler(webapp2.RequestHandler):
     """
     def get(self):
         """Get all tasks in json form"""
-        tasks = [{'id': task.key.id(), 'content': task.content}
+        tasks = [
+            {'id': task.key.id(), 'content': task.content}
             for task in Task.get_tasks(get_task_list_key())
         ]
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(tasks))
+
 
 class TaskHandler(webapp2.RequestHandler):
     """Handle one particular task
